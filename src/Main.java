864d1daa1b29adc49d680b6c926f1628a4b6f437
Luca Utzeri <lutzeri@libero.it>
2024-01-21 16:20:07 +0100

    many errors shown

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArchivioBibliotecario archivio = new ArchivioBibliotecario();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Libro libro1 = new Libro("123456", "Il Signore degli Anelli", 1954, 1000, "J.R.R. Tolkien", "Fantasy");
+        Libro libro2 = new Libro("789012", "1984", 1949, 300, "George Orwell", "Distopia");
+
+        Rivista rivista1 = new Rivista("345678", "National Geographic", 2022, 50, Periodicita.MENSILE);
+        Rivista rivista2 = new Rivista("901234", "Time", 2022, 60, Periodicita.SETTIMANALE);
+
+        archivio.aggiungiElemento(libro1);
+        archivio.aggiungiElemento(libro2);
+        archivio.aggiungiElemento(rivista1);
+        archivio.aggiungiElemento(rivista2);
+
+        archivio.ricercaPerAutore("J.R.R. Tolkien").forEach(System.out::println);
+
+        archivio.rimuoviElemento("789012");
+
+        archivio.ricercaPerAnnoPubblicazione(2022).forEach(System.out::println);
+
+        archivio.salvaSuDisco("archivio.dat");
+
+        ArchivioBibliotecario archivioCaricato = new ArchivioBibliotecario();
+        archivioCaricato.caricaDaDisco("archivio.dat");
+        archivioCaricato.catalogo.forEach(System.out::println);
     }
 }
